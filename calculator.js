52be152dae1a7176a6c52481a569018b85d68592
Deluxateer <deluxateer@gmail.com>
2020-01-09 13:02:45 -0600

    add basic adding functionality

--- calculator.js
+++ calculator.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+  const result = parseFloat(req.body.num1) + parseFloat(req.body.num2);
+
+  res.send(`Result: ${result}`);
 });
 
 const port = 3000;
