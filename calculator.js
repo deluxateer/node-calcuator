e5ca95ec15c1eb106c4aa0fb36f1b99a7d5b15b4
Deluxateer <deluxateer@gmail.com>
2020-01-09 13:31:07 -0600

    add bmiCalculator functionality

--- calculator.js
+++ calculator.js
@@ -8,10 +8,22 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/bmicalculator', function(req, res) {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
 app.post('/', function(req, res) {
   const result = parseFloat(req.body.num1) + parseFloat(req.body.num2);
 
   res.send(`Result: ${result}`);
+});
+
+app.post('/bmicalculator', function(req, res) {
+  const weight = parseFloat(req.body.weight);
+  const height = parseFloat(req.body.height);
+  const bmi = weight / (height * height);
+
+  res.send(`Your BMI is ${bmi}`);
 });
 
 const port = 3000;
